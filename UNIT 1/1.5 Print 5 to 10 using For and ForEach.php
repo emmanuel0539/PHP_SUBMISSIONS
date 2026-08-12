ee788d51f7eec486000749c49aec187263b1e896
Emmanuel Onah <emmanuel0539@gmail.com>
2026-08-12 12:19:15 +0530

    Add 1.5 for and foreach loop program

---
+++ UNIT 1/1.5 Print 5 to 10 using For and ForEach.php
@@ -0,0 +1,17 @@
+<?php
+
+for ($i = 5; $i <= 10; $i++) {
+    echo $i . "<br>";
+}
+
+?>
+
+<?php
+
+$numbers = [5, 6, 7, 8, 9, 10];
+
+foreach ($numbers as $number) {
+    echo $number . "<br>";
+}
+
+?>
