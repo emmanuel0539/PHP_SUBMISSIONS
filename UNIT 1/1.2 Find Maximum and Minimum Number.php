af825ec6d0300362d409717a86044eeb90a40e3e
Emmanuel Onah <emmanuel0539@gmail.com>
2026-08-12 12:11:49 +0530

    Add 1.2 maximum and minimum number program

---
+++ UNIT 1/1.2 Find Maximum and Minimum Number.php
@@ -0,0 +1,16 @@
+<?php
+
+$num1 = 25;
+$num2 = 10;
+$num3 = 40;
+$num4 = 15;
+
+$maximum = max($num1, $num2, $num3, $num4);
+$minimum = min($num1, $num2, $num3, $num4);
+
+echo "Numbers are: $num1, $num2, $num3, $num4 <br><br>";
+
+echo "Maximum Number: " . $maximum . "<br>";
+echo "Minimum Number: " . $minimum;
+
+?>
