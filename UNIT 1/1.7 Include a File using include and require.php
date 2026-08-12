169483c0dda7a3e935714ae1707aa2e3c49172ac
Emmanuel Onah <emmanuel0539@gmail.com>
2026-08-12 12:20:03 +0530

    Add 1.7 include and require program

---
+++ UNIT 1/1.7 Include a File using include and require.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h2>PHP Include and Require Example</h2>";
+
+echo "Using Include:<br>";
+include "message.php";
+
+echo "<br>";
+
+echo "Using Require:<br>";
+require "message.php";
+
+?>
